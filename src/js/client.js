68197568a2e80b0b46621466c351afa72ecf5466
nikkifargus <nikki@fargus.co.uk>
2017-07-27 10:08:56 +0100

    added click event

--- src/js/client.js
+++ src/js/client.js
@@ -1,24 +1,21 @@
 console.log('client log!');
 
-// $
-// .get('http://api.yummly.com/v1/api/recipes?_app_id=336eb706&_app_key=2345f4bf66c4925c8eef59b889c4816d&q=&allowedCourse[]=course^course-Desserts&maxResult=50', function(data,i){
-//   for (i = 0; i < data.matches.length; i++) {
-//     console.log(data.matches[i].recipeName);
-//   }
-//
-//   for (i = 0; i < data.matches.length; i++) {
-//     $('.inspirationLoad').text(data.matches[i].recipeName);
-//   }
-// });
-
 $(() => {
+  $('.yummlyRecipe').hide();
   $
   .get('http://api.yummly.com/v1/api/recipes?_app_id=336eb706&_app_key=2345f4bf66c4925c8eef59b889c4816d&q=&allowedCourse[]=course^course-Desserts&maxResult=50')
   .done(data => {
     data.matches.forEach(recipe => {
       $(`<div><div class="inspirationPhoto"><img src="${ recipe.smallImageUrls}"></div><div class="inspirationName">${recipe.recipeName}</div></div>`).appendTo($('.inspirationLoad'));
     });
+    $('.inspirationName').on('click', function(){
+      console.log('clicked');
+    });
   })
   .fail(() => console.log('error'));
-
 });
+
+
+//
+// .get(`http://api.yummly.com/v1/api/recipe/${recipe.Id}?_app_id=336eb706&_app_key=2345f4bf66c4925c8eef59b889c4816d`)
+// .done.forEach(recipe =>{})
